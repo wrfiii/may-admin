77e0b4d455d4b9790617fde229b9815bdbb93d2e
LifePureDream <18982827217>
2024-03-29 18:29:54 +0800

    utils

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ElButton } from "element-plus";
+</script>
+
+<template>
+  <div>
+    <el-button>我是 ElButton</el-button>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
